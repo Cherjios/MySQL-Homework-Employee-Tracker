e7480bf42aed413258ca1339384392346ca88509
cherjios <slopez0286@gmail.com>
2020-07-20 22:53:12 -0700

    Fixing tables

--- seed.sql
+++ seed.sql
@@ -6,13 +6,13 @@ USE companyDB;
 
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NULL,
+    name VARCHAR(30) NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE rol(
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(100) NULL,
+    title VARCHAR(30) NULL,
     salary DECIMAL,
     department_id INT REFERENCES deparment (id),
     PRIMARY KEY (id)
@@ -20,9 +20,10 @@ CREATE TABLE rol(
 
 CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT,
-    fist_name VARCHAR(100) NULL,
-    last_name VARCHAR(100) NULL,
+    fist_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
     role_id INT REFERENCES rol (id),
     manager_id INT REFERENCES employee (id) NULL ,
     PRIMARY KEY (id)
 );
+
