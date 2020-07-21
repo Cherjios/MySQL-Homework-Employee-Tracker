913d2e152ba66ea8096f39187edaff7f0c5c1ff2
cherjios <slopez0286@gmail.com>
2020-07-20 22:50:35 -0700

    Create employee table

--- seed.sql
+++ seed.sql
@@ -13,6 +13,16 @@ CREATE TABLE department(
 CREATE TABLE rol(
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(100) NULL,
-    department_id INT NULL,
+    salary DECIMAL,
+    department_id INT REFERENCES deparment (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    fist_name VARCHAR(100) NULL,
+    last_name VARCHAR(100) NULL,
+    role_id INT REFERENCES rol (id),
+    manager_id INT REFERENCES employee (id) NULL ,
     PRIMARY KEY (id)
 );
